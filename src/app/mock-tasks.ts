fd1d3c074c519cacfa59213965eeed07317e6d1c
Jamal Haider <77342903+jamalhyderrr@users.noreply.github.com>
2022-02-19 12:43:01 +0500

    Task Tracker with View

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS:Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:00pm',
+        reminder:true
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'May 6th at 1:30pm',
+        reminder:true
+    },
+    {
+        id: 1,
+        text: 'Food Shopping',
+        day: 'May 7th at 12:30pm',
+        reminder:false
+    }
+]
